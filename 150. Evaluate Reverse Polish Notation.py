e5da8420dbfd98f33f237e2f9915dfcdbd353933
codersudharshan <sudharshanv566@gmail.com>
2025-08-10 21:20:34 +0530

    Added new solution for problem 150 - Evaluate Reverse Polish Notation

--- 150. Evaluate Reverse Polish Notation.py
+++ 150. Evaluate Reverse Polish Notation.py
@@ -15,3 +15,31 @@ class Solution:
             else:
                 stack.append(int(c))
         return stack[0]
+    
+#new solution :
+from math import ceil , floor
+from typing import List
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stk = []
+        for t in tokens:
+            if t in '+-*/':
+                b, a = stk.pop(), stk.pop()
+
+                if t == '+':
+                    stk.append(a + b)
+                elif t == '-':
+                    stk.append(a - b)
+                elif t == '*':
+                    stk.append(a * b)
+                else:
+                    division = a / b
+                    if division < 0:
+                        stk.append(ceil(division))
+                    else:
+                        stk.append(floor(division))
+            
+            else:
+                stk.append(int(t))
+        
+        return stk[0]
